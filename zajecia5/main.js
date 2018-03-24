84df80997c4f1d775db0ab37c4d4b1ac5b64d792
kacperbin <37475736+kacperbin@users.noreply.github.com>
2018-03-24 14:59:51 +0100

    Add files via upload

---
+++ zajecia5/main.js
@@ -0,0 +1,16 @@
+//var name = prompt("Jak masz na imię?");
+//alert("Hej " +name)
+
+//var wiek = prompt("Ile masz lat?")
+
+var klik  = function(){
+
+	document.querySelector('main').style.color = 'blue';
+
+}
+
+var unclick = function(){
+
+	document.querySelector('main').style.color = 'black';
+}
+
