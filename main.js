a2dc97c679c380091313cf56d541ce41b6f157e4
kacperbin <37475736+kacperbin@users.noreply.github.com>
2018-05-12 12:06:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+var wys = 100;
+var dlu = 100;
+
+
+var klik  = function(){
+
+	document.querySelector('main').style.color = 'blue';
+
+}
+
+var unclick = function(){
+
+
+	document.querySelector('main').style.color = 'black';
+}
+
+function plus(w,d) {
+	wys += w;
+	dlu += d;
+	var tw = wys + 'px';
+	var td = dlu +'px';
+
+	document.querySelector('img').style.height = tw;
+	document.querySelector('img').style.width = td;
+}
+
+function szukaj() {
+	var zapytanie = document.getElementById('zapytanie').value;
+	console.log(zapytanie)
+	var link = 'https://maps.google.com/?q=';
+	window.open(link+zapytanie);
+}
+
