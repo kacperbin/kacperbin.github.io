0e3615429ac972ebea0e3cb3cb8fcf6ab772648b
kacperbin <37475736+kacperbin@users.noreply.github.com>
2018-05-12 12:03:54 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var name = prompt("Jak masz na imię?");
-alert("Hej " +name)
 
-var wiek = prompt("Ile masz lat?")
+
+var wys = 100;
+var dlu = 100;
+
 
 var klik  = function(){
 
@@ -11,8 +12,24 @@ var klik  = function(){
 
 var unclick = function(){
 
+
 	document.querySelector('main').style.color = 'black';
 }
 
+function plus(w,d) {
+	wys += w;
+	dlu += d;
+	var tw = wys + 'px';
+	var td = dlu +'px';
 
+	document.querySelector('img').style.height = tw;
+	document.querySelector('img').style.width = td;
+}
+
+function szukaj() {
+	var zapytanie = document.getElementById('zapytanie').value;
+	console.log(zapytanie)
+	var link = 'https://maps.google.com/?q=';
+	window.open(link+zapytanie);
+}
 
